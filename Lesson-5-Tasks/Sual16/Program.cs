c1c2209597f6bcb96a340a0e27dc5f536318713d
Rufat Ali <90063086+Rufat2314@users.noreply.github.com>
2022-08-21 13:52:21 +0400

    16ci ders

---
+++ Lesson-5-Tasks/Sual16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sual16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numb = new double[] { 189.258, 58.987, 78, 1589, 5523.65 };
+            double[] numbMultiplied = new double[numb.Length];
+
+            // "numb" elements multiplied by 2
+            for (int i = 0; i < numb.Length; i++)
+            {
+                numbMultiplied[i] = numb[i] * 2;
+            }
+
+            // Read "numbMultiplied"
+            for (int a = 0; a < numbMultiplied.Length; a++)
+            {
+                Console.WriteLine(numbMultiplied[a]);
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
