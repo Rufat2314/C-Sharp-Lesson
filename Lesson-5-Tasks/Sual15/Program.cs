20a001e7e53d799c25f2deec53f52255732b806a
Rufat Ali <90063086+Rufat2314@users.noreply.github.com>
2022-08-21 13:24:44 +0400

    15ci tapsiriq

---
+++ Lesson-5-Tasks/Sual15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sual15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new string[] {"Nurlan", "Haci" };
+            
+            bool check = str[0] is string;
+
+            if (check)
+            {
+                Console.WriteLine(str.GetLength(0));
+            }
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
