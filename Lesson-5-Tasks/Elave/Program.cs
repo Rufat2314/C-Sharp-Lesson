a167a6a96626a0754e607dccab9fab439b15cc16
Rufat Ali <90063086+Rufat2314@users.noreply.github.com>
2022-08-22 15:37:01 +0400

    Addition algorithm

---
+++ Lesson-5-Tasks/Elave/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add even numbers to array
+            int[] arr = new int[10];
+
+            int count = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arr[count] = i;
+                    count++;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+            // Find Min and Max elementlerin tapilmasi
+
+            int r = arr[0];  // 2 4 6 8 10
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (r < arr[i])
+                {
+                    r = arr[i];
+                }
+                
+            }
+
+
+            Console.WriteLine(r);
+
+            Console.ReadKey();
+        }
+    }
+}
